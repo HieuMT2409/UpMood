9f09b67c4428f840c5a3c549bff4be3f6ef9613a
HieuMT249 <minhhieu1002cm@gmail.com>
2023-04-09 23:01:35 +0700

    upmood_version_1.4

--- app/src/main/java/com/example/upmood/SignInActivity.java
+++ app/src/main/java/com/example/upmood/SignInActivity.java
@@ -4,24 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInActivity extends AppCompatActivity {
 
-    EditText edtUsername,edtPassword;
-    Button btnForgot,btnSignIn,btnFacebook,btnGoogle;
+    private FirebaseAuth auth;
+    private EditText edtUsername,edtPassword;
+    private Button btnForgot,btnSignIn,btnFacebook,btnGoogle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
 
+        auth = FirebaseAuth.getInstance();
         // anh xa view
         edtUsername = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
@@ -30,6 +39,41 @@ public class SignInActivity extends AppCompatActivity {
         btnFacebook = findViewById(R.id.btnFacebook);
         btnGoogle = findViewById(R.id.btnGoogle);
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                //xu ly dang nhap
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if(!password.isEmpty()){
+                        auth.signInWithEmailAndPassword(email, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        //neu dang nhap thanh cong thi chuyen sang trang home
+                                        Toast.makeText(SignInActivity.this, "Đănh nhập thành công", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(SignInActivity.this,MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(SignInActivity.this, "Đănh nhập không thành công", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else{
+                        //sai mat khau
+                        edtPassword.setError("Mật khẩu không chính xác");
+                    }
+                }else if(email.isEmpty()){
+                        edtUsername.setError("Email không được bỏ trống");
+                }else{
+                    edtUsername.setError("Email không chính xác");
+                }
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/upmood/SignUpActivity.java
+++ app/src/main/java/com/example/upmood/SignUpActivity.java
@@ -1,29 +1,82 @@
 package com.example.upmood;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText edtUsername,edtPassword,edtConfirmPassword;
-    Button btnSignup;
+    private FirebaseAuth auth;
+    private EditText edtEmailSignUp,edtPasswordSignUp,edtConfirmPasswordSignUp,edtPhone;
+    private Button btnSignup;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_signup);
 
-        edtUsername = findViewById(R.id.edtUsername);
-        edtPassword = findViewById(R.id.edtPassword);
-        edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
+        //khai bao firebase
+        auth = FirebaseAuth.getInstance();
+
+        //anh xa view tu fragment_sign_up
+        edtEmailSignUp = findViewById(R.id.edtEmailSignUp);
+        edtPhone = findViewById(R.id.edtPhone);
+        edtPasswordSignUp = findViewById(R.id.edtPasswordSignUp);
+        edtConfirmPasswordSignUp = findViewById(R.id.edtConfirmPasswordSignUp);
         btnSignup = findViewById(R.id.btnSignUp);
+
+        //xu ly nut Sign Up
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("dangki","DAng ki");
+                String email = edtEmailSignUp.getText().toString().trim();
+                String password = edtPasswordSignUp.getText().toString().trim();
+                String re_password = edtConfirmPasswordSignUp.getText().toString().trim();
+
+                //xu ly khi o email trong
+                if(email.isEmpty()){
+                    edtEmailSignUp.setError("Email không được để trống");
+                }
+
+                //xu ly khi o password trong
+                if(password.isEmpty()){
+                    edtPasswordSignUp.setError("Password không được để trống");
+                }else {
+                    if(password.equals(re_password)){
+                        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(SignUpActivity.this, "Đăng ký thành công !", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+                                }else {
+                                    Toast.makeText(SignUpActivity.this, "Đăng ký thất bại !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }else {
+                        Toast.makeText(SignUpActivity.this, "Mật khẩu nhập lại không chính xác !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
