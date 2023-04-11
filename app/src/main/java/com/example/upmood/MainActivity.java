198313955f844b12ce55781ce507980bae3be38c
HieuMT249 <minhhieu1002cm@gmail.com>
2023-04-11 23:39:27 +0700

    upmood_version_1.4.2

--- app/src/main/java/com/example/upmood/MainActivity.java
+++ app/src/main/java/com/example/upmood/MainActivity.java
@@ -1,33 +1,58 @@
 package com.example.upmood;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.upmood.databinding.ActivityMainBinding;
+import com.example.upmood.fragment_nav.ProfileFragment;
+import com.example.upmood.fragment_nav.SettingFragment;
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
-    BottomNavigationView navigationView;
-    Button btn_user;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private ActivityMainBinding binding;
+    private Button btn_user;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolBar;
+    private NavigationView navigationView;
+    private AppBarLayout appBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -52,13 +77,21 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        //xu ly action bar
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-
         //xu ly cac su kien header
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolBar = findViewById(R.id.toolBar);
+
+        //add toolbar va su kien dong/mo navigation
+        setSupportActionBar(toolBar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolBar,R.string.open_nav_drawer,R.string.close_nav_drawer);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.primary));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //xu ly su kien tren navigation
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -68,6 +101,45 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    //hien thi menu va xu ly khi nhan nut
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_notify,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    //ham xu ly cac su kien trong navigation
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.my_profile:
+                replaceFragment(new ProfileFragment());
+                break;
+            case R.id.setting:
+                replaceFragment(new SettingFragment());
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //xu ly nut back ve
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
